630b7c37e1271f5132250fb647a6f2cd7aa7f060
sebastianakampka-maker <sebastian.a.kampka@gmail.com>
2026-04-08 14:32:22 +0200

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{padding: "40px", color: "white"}}>
+      <h1>OmniWorks läuft 🚀</h1>
+      <p>Website ist online</p>
+    </div>
+  );
+}
